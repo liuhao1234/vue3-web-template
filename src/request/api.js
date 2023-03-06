b66a90e50633ec7f17286e36ea383bc69177cbbb
liu-hao_neu <liu-hao_neu@neusoft.com>
2023-03-06 13:14:52 +0800

    feat: initial

--- src/request/api.js
+++ src/request/api.js
@@ -1,10 +1,7 @@
-import {
-	get,
-	post 
-} from '@/request/http.js'
+import { get, post } from '@/request/http.js';
 
 // 登录页面api
-export const login = params => post('/api/login', params)
-export const getVerifyCode = () => get('/api/searchVerifyCode')
-export const getNoticeList = (params) => get('/api/noticeinfo/list',params)
-export const getEventDetail = (params) => get(`/api/eventinfo/info/${params.etId}`)
+export const login = (params) => post('/api/login', params);
+export const getVerifyCode = () => get('/api/searchVerifyCode');
+export const getNoticeList = (params) => get('/api/noticeinfo/list', params);
+export const getEventDetail = (params) => get(`/api/eventinfo/info/${params.etId}`);
